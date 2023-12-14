ffcef51aff95e5a1bebe57597bc4f33b2c2a9057
yunyun415 <151757886+yunyun415@users.noreply.github.com>
2023-12-14 18:28:42 +0800

    Create main1.c

---
+++ hw5/main1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 7;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j <= n - i)
+                printf(" ");
+            else
+                printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
