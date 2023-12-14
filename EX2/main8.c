e35337783bed8c3f2f0f32eff312073e1824a975
yunyun415 <151757886+yunyun415@users.noreply.github.com>
2023-12-14 15:31:48 +0800

    Create main8.c

---
+++ EX2/main8.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int x = 9;
+    int y = 9;
+    int z = 9;
+    if(x<0){
+        int sum =(x*-1)*100+y*10+z;
+        printf("%d",sum*-1);
+    }
+    else{
+         printf("%d",x*100+y*10+z);
+    }
+        
+}
