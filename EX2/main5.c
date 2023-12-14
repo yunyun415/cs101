9df7d337bb0bbc3b37e02df7fdd8ba5c1703bc5a
yunyun415 <151757886+yunyun415@users.noreply.github.com>
2023-12-14 15:19:59 +0800

    Create main5.c

---
+++ EX2/main5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 119;
+    if(i<=30){
+        printf("免費");
+    }
+    else if(i>=240){
+        printf("240元");
+    }
+    else {
+        printf("%d元",i);
+    }
+    }
