4264edf6fb29ffa1aa12a583e3850e0f97bbdc7a
yunyun415 <151757886+yunyun415@users.noreply.github.com>
2023-12-14 15:08:22 +0800

    Update main2.c

--- EX2/main2.c
+++ EX2/main2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int i = 9;
+    if (i % 15 ==0){
+        printf("LOVE IU");
+    }
+    else if (i % 3 ==0){
+        printf("LOVE");
+    }
+    else if (i % 5 ==0){
+        printf("IU");
+    }
+    else{
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
